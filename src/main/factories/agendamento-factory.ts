744429dd8b8270963690732b7d8132f78f589ad4
breno <brenoferreira.bf8@gmail.com>
2026-05-06 18:33:49 -0400

    feat: add agendamento handlers and factory for creating and listing agendas
    - Implemented create-agendamento handler to manage scheduling requests.
    - Added get-agenda handler to retrieve scheduled agendas.
    - Created agendamento factory to instantiate use cases with memory repositories.
    - Updated package.json to include new scripts for development and testing.
    - Downgraded serverless version for compatibility.

---
+++ src/main/factories/agendamento-factory.ts
@@ -0,0 +1,12 @@
+import { CriarAgendamentoUseCase } from '@/application/use-cases/criar-agendamento.use-case';
+import { ListarAgendasUseCase } from '@/application/use-cases/listar-agendas.use-case';
+import { MemoryAgendamentoRepository } from '@/infra/persistence/memory/memory-agendamento.repository';
+import { MemoryMedicoRepository } from '@/infra/persistence/memory/memory-medico.repository';
+
+const medicoRepo = new MemoryMedicoRepository();
+const agendamentoRepo = new MemoryAgendamentoRepository();
+
+export const makeListarAgendasUseCase = () => new ListarAgendasUseCase(medicoRepo);
+
+export const makeCriarAgendamentoUseCase = () => 
+  new CriarAgendamentoUseCase(agendamentoRepo, medicoRepo);
